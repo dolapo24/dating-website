7d937089a791359195856cb4bae5adb9d316cdec
omodolapo <anisereomodolapo51@gmail.com>
2023-08-18 11:51:00 -0700

    Add about.html files

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       if (gender === 'male') {
         window.location = "/female.html"
       }else {
-        window.location = "/male.html"
+        window.locati = "/male.html"
       }
 
       e.preventDefault();
